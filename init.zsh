a2ba083e95139679aa47d8e98cc4ae10243afe34
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-03-03 09:15:12 -0500

    fix: vs code extension comet.* is toast

--- init.zsh
+++ init.zsh
@@ -34,7 +34,7 @@ p6df::modules::comet::langs() {
 #
 # Function: p6df::modules::comet::init()
 #
-#  Environment:	 COMET_API_KEY COMET_LOGGING_FILE COMET_LOGGING_FILE_LEVEL P6_DFZ_SRC_DIR
+#  Environment:	 COMET_API_KEY COMET_LOGGING_FILE COMET_LOGGING_FILE_LEVEL
 #>
 ######################################################################
 p6df::modules::comet::init() {
@@ -42,7 +42,6 @@ p6df::modules::comet::init() {
   p6_env_export "COMET_LOGGING_FILE_LEVEL" "debug"
   p6_env_export "COMET_LOGGING_FILE" "/tmp/comet.debug.log"
 
-  p6_file_load "$P6_DFZ_SRC_DIR/pgollucci/home-private/cr/comet/token"
   p6_env_export "COMET_API_KEY" "$P6_COMET_API_KEY"
 
   p6_return_void
@@ -56,28 +55,18 @@ p6df::modules::comet::init() {
 #  Returns:
 #	str - str
 #
-#  Environment:	 P6_COMET_WORKSPACE
+#  Environment:	 P6_COMET_USER P6_COMET_WORKSPACE
 #>
 ######################################################################
 p6df::modules::comet::prompt::line() {
 
-  local str="comet:\t\t  w:$P6_COMET_WORKSPACE u:$P6_COMET_USER"
+  local str=""
+
+  if ! p6_string_blank "$P6_COMET_USER"; then
+    str="comet:\t\t  w:$P6_COMET_WORKSPACE u:$P6_COMET_USER"
+  fi
 
   p6_return_str "$str"
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::comet::vscodes()
-#
-#>
-######################################################################
-p6df::modules::comet::vscodes() {
-
-  code --install-extension comet.ml.comet
-
-  p6_return_void
 }
 
 ######################################################################
@@ -99,4 +88,3 @@ p6df::modules::comet::clones() {
 # https://github.com/comet-ml/issue-tracking
 # https://github.com/comet-ml/comet-examples
 # https://www.comet.ml/docs/
-
